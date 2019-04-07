f325ca19d3a302f6072cde00f6636f66968ade0c
Ahmed Ali <ahmed.alawadly@gmail.com>
2019-04-07 16:11:42 +0200

    add scheduled task to fetch the rss feed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,10 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Feeds;
+use Log;
+use Cache;
+use Carbon;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +28,31 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function (){
+            $feed = Feeds::make('https://www.winespectator.com/rss/rss?t=dwp');
+            Cache::flush();
+            
+            foreach($feed->get_items()as $item){
+                $date =  Carbon::parse($item->get_date())->startOfDay();
+                if($date->eq(Carbon::today())){
+                    Log::debug($item->get_title());
+                    $data = [
+                        'title' => $item->get_title(),
+                        'pubDate' => $item->get_date(),
+                        'description' => $item->get_description(),
+                        'link' => $item->get_link(),
+                        'category' => $item->get_category(),
+                        'author' => $item->get_author()
+                    ];
+                    
+                    Cache::put($item->get_title(), $data , now()->addDays(1));
+                    Log::debug($item->get_title());
+                } 
+            };
+            
+            Log::debug(count($feed->get_items()));
+        
+        })->dailyAt('6:00');
     }
 
     /**
