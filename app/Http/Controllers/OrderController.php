2091fdaecaad371d44a56126559ef095c1e5ea02
Ahmed Ali <ahmed.alawadly@gmail.com>
2019-04-07 17:00:01 +0200

    implement /order api

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use Cache;
+
+class OrderController extends Controller
+{
+    public function makeOrder(Request $request){
+        $validator = Validator::make($request->all(), [
+            'wine_title' => 'required|string'
+            ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        $wine_title = $request->wine_title;
+
+        $wine = Cache::get($wine_title);
+
+        if(empty($wine)){
+            return response()->json([
+                'message' => 'Error: Wine not availabe today.'
+            ], 404);
+        }
+
+        return response()->json([
+            'data' => $wine,
+            'message' => 'Succes'
+        ], 200);
+    }
+}
